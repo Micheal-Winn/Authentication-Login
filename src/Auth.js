2cef7d45f977684250805b07667fb4524ecb230a
Micheal-Winn <thant.zin.winnnn@gmail.com>
2022-09-19 11:28:13 +0630

    Login and Register

---
+++ src/Auth.js
@@ -0,0 +1,79 @@
+import React,{useState} from 'react';
+import {  Box, Button,  TextField, Typography } from '@mui/material';
+import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
+import HowToRegOutlinedIcon from '@mui/icons-material/HowToRegOutlined';
+
+
+export const Auth = () => {
+    const [isSignup, setisSignup] = useState(false);
+    console.log(isSignup)
+
+    const [input, setinput] = useState({
+        name: '', email: '', password:''
+    })
+
+    const handleChange = (e)=>{
+        setinput(prevState=>({
+            ...prevState,
+            [e.target.name] : e.target.value
+        }))
+    }
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        console.log(input)
+    }
+
+    const resetState = ()=>{
+        setisSignup(!isSignup);
+        setinput({name: '', email:'',password:''})
+    }
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <Box
+            display={"flex"}
+             flexDirection={"column"}
+             maxWidth={400}
+             alignItems="center" 
+            justifyContent={"center"}
+            margin="auto" marginTop={7} padding={3} borderRadius={5} boxShadow={"5px 5px 10px #ccc"}
+            sx={{
+                ":hover": {
+                    boxShadow:"10px 10px 20px #ccc"
+                }
+            }}>
+                <Typography variant='h2' padding={3} textAlign="center">
+                    {isSignup ? "signup" : "login"}
+                </Typography>
+               {isSignup && <TextField name='name'
+               onChange={handleChange}
+                value={input.name}
+                margin='normal'
+                type={'text'}
+                variant='outlined'
+                placeholder='Name'/>}
+                <TextField name='email' value={input.email}
+                onChange={handleChange}
+                 margin='normal'
+                 type={'email'}
+                 variant='outlined'
+                 placeholder='Email'/>
+                <TextField name='password'
+                 value={input.password}
+                 onChange={handleChange}
+                 margin='normal'
+                 type={'password'}
+                 variant='outlined'
+                 placeholder='Password'/>
+                <Button endIcon={isSignup ? <HowToRegOutlinedIcon/> : <LoginOutlinedIcon/>} type='submit' variant='contained' color='warning' sx={{marginTop: 3,borderRadius:3}}>
+                {isSignup ? "signup" : "login"}
+                </Button>
+                <Button  sx={{marginTop: 3,borderRadius:3}} onClick={resetState}>
+                    Change to {isSignup ? "login" : "signup"}
+                </Button>
+            </Box>
+        </form>
+    </div>
+  )
+}
